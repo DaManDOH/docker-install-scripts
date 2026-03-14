75a23664e701676ea352b7a3380cb21735a2588b
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2026-03-14 14:42:02 -0400

    Update 02_install_latest_packages.sh

--- 02_install_latest_packages.sh
+++ 02_install_latest_packages.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-sudo apt-get update \
-  && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo bash -c "apt update && \
+    apt install \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin \
+        docker-compose-plugin"
