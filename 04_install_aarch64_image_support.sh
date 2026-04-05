185ccc0e07b1891f3dde5eb5880f0f1158e8fde7
Dan C. Wlodarski <dan.wlodarski@gmail.com>
2026-04-05 10:34:03 -0400

    Update 04_install_aarch64_image_support.sh

--- 04_install_aarch64_image_support.sh
+++ 04_install_aarch64_image_support.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt -y install qemu-user-static binfmt-support
 docker run --privileged --rm tonistiigi/binfmt --install all
+docker image rm tonistiigi/binfmt
